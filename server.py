4f0f62517d5b2c1e5fcedc620cc08cd57adb0a81
Derek G Chen <derek@derekgc.com>
2020-06-04 20:01:06 -0400

    finished up the post get delete users

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 # using flask_restful 
 from flask import Flask, jsonify, request 
 from flask_restful import Resource, Api 
-from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo, ObjectId
   
 # creating the flask app 
 app = Flask(__name__) 
@@ -34,34 +34,62 @@ class Hello(Resource):
   
   
 # another resource to calculate the square of a number 
-class Square(Resource): 
+class All_user_id(Resource): 
   
-    def get(self, num): 
+    def get(self): 
   
-        return jsonify({'square': num**2}) 
+        return jsonify() 
 
 class User(Resource):
     
-    def get(self, user_id):
-        found = db.users.find_one({"_id":user_id})
-        return jsonify(found)
+    def get(self, user_info):
+        found = self.find_user(user_info)
+        
+        return jsonify({"response":found})
     
-    def post(self, user_id):
-        userid = db.users.insert_one({"_id":user_id, "name":"derek"})
-        print('message:=====', user_id)
-        return jsonify(userid)
+    def post(self, user_info):
+        data = request.get_json()
+        
+        if not data:
+            return jsonify({"response":"ERROR. Empty payload"})
+        else:
+            username = data.get('username')
+            if db.users.find_one({'username':username}):
+                return jsonify({"response":"ERROR. Already exisits"})
+            else:
+                db.users.insert_one(data)
+                found = self.find_user(username)
+                return jsonify({"response":found})
+                
     
-    def delete(self, user_id):
-        return jsonify({'delete user':'derek', 'user_id':user_id})
+    def delete(self, user_info):
+        found = self.find_user(user_info)
+        if not found:
+            return jsonify({'response':"user not found", 'user':user_info})
+        result = db.users.delete_one({'_id':ObjectId(found['_id'])})
+        
+        print('result======', {'_id':found['_id']})
+        print('result======', result.acknowledged, result.deleted_count)
+        return jsonify({'response':'user deteled', 'user': user_info})
     
     def patch(self, user_id):
         return jsonify({'patch user':'derek', 'user_id':user_id})
+    
+    def find_user(self, user_info):
+        found = db.users.find_one({"_id":user_info})
+        if not found:
+            found = db.users.find_one({"username":user_info})
+        
+        if found:
+            print('message:=====', found)
+            found['_id'] = str(found['_id'])
+        return found
         
   
 # adding the defined resources along with their corresponding urls 
 api.add_resource(Hello, '/') 
-api.add_resource(Square, '/square/<int:num>') 
-api.add_resource(User, '/api/user/<string:user_id>') 
+api.add_resource(All_user_id, '/api/all') 
+api.add_resource(User, '/api/user/<string:user_info>') 
   
   
 # driver function 
