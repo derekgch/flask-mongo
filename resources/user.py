37fd724ebf9742f41b0fa0c9c35641d78cfc9621
Derek G Chen <derek@derekgc.com>
2020-06-18 20:46:50 -0400

    user database connection is up

--- resources/user.py
+++ resources/user.py
@@ -6,10 +6,7 @@ from flask_restful import Resource
 from flask_pymongo import ObjectId
 from database.db import database, bcrypt
 
-db = database.db
-
-class User(Resource):
-    
+class User(Resource):    
     def get(self, user_info):
         found = self.find_user(user_info)
         
@@ -22,11 +19,11 @@ class User(Resource):
             return jsonify({"response":"ERROR. Empty payload"})
         else:
             username = data.get('username')
-            if db.users.find_one({'username':username}):
+            if database.db.users.find_one({'username':username}):
                 return jsonify({"response":"ERROR. Already exisits"})
             else:
                 data_hashed = self.hash_password(data)
-                db.users.insert_one(data_hashed)
+                database.db.users.insert_one(data_hashed)
                 found = self.find_user(username)
                 
                 return jsonify({"response":self.trip_pw_hash(found)})
@@ -36,7 +33,7 @@ class User(Resource):
         found = self.find_user(user_info)
         if not found:
             return jsonify({'response':"user not found", 'user':user_info})
-        db.users.delete_one({'_id':ObjectId(found['_id'])})
+        database.db.users.delete_one({'_id':ObjectId(found['_id'])})
         
         return jsonify({'response':'user deteled', 'user': user_info})
     
@@ -46,13 +43,14 @@ class User(Resource):
     def find_user(self, user_info):
         found = False
         if(ObjectId.is_valid(user_info)):
-            found = db.users.find_one({"_id":ObjectId(user_info)})
+            found = database.db.users.find_one({"_id":ObjectId(user_info)})
             
         if not found:
-            found = db.users.find_one({"username":user_info})
+            found = database.db.users.find_one({"username":user_info})
         
         if found:
             found['_id'] = str(found['_id'])
+            found['password'] = "****"
         return found
     
     def hash_password(self, data):
